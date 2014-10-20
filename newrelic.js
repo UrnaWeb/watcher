6636a829e79ad53814c3b71aa986eaf02fc9baee
Kaue Miyauti Machado <kaumac@gmail.com>
2014-10-20 08:57:21 -0200

    config

--- newrelic.js
+++ newrelic.js
@@ -8,11 +8,11 @@ exports.config = {
   /**
    * Array of application names.
    */
-  // app_name : ['My Application'],
+  app_name : ['UrnaWebWatcher'],
   /**
    * Your New Relic license key.
    */
-  // license_key : 'license key here',
+  license_key : '67c20c1836ea9965ada76df7d9c6c16b6bcb837f',
   logging : {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
